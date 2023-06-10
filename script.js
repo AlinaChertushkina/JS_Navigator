62c9aaa5aca4b18f051cba6c3e48dde148a4c396
Alina <126471525+AlinaChertushkina@users.noreply.github.com>
2023-06-10 13:34:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let time = 34;
+console.log(time)
+
+console.log('Старт поездки. Осталось минут: ' + time)
+
+let time = 15;
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time)
+
+let time = time - 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time)
+
+let time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву')
